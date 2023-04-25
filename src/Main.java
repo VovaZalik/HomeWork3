b90b34468c0deadc6847c09704f1d6a057d86911
VovaZ <zalikvova1998@gmail.com>
2023-04-25 11:25:07 +0300

    Refactoring

--- src/Main.java
+++ src/Main.java
@@ -2,54 +2,61 @@ import java.util.*;
 
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner ss = new Scanner(System.in);
         ArrayList<String> products = new ArrayList<>();
-        ArrayList<String> search = new ArrayList<>();
-
         while (true) {
             menu();
             String input = ss.nextLine();
-
             switch (input) {
                 case ("1"): {
-                    System.out.println("Какую покупку добавить ?");
-                    products.add(ss.nextLine());
-                    System.out.println("Итого в списке покупок: " + products.size());
-                    System.out.println();
+                    addProduct(ss, products);
                     break;
                 }
-
                 case ("2"): {
                     shoppingList(products);
                     break;
                 }
                 case ("3"): {
-                    shoppingList(products);
-                    System.out.println("Какую хотите удалить? Введите номер или название.");
-                    String operation = ss.next();
-                    try {
-                        int number = Integer.parseInt(operation);
-                        products.remove(number - 1);
-                    } catch (Exception e) {
-                        products.remove(operation);
-                    }
+                    deleteProduct(ss, products);
                     break;
                 }
                 case ("4"): {
-                    String pop = ss.nextLine();
-                    System.out.println("Найдено:");
-                    for (int i = 0; i < products.size(); i++) {
-                        if (products.get(i).toLowerCase().contains(pop.toLowerCase())) {
-                            search.add(products.get(i));
-                        }
-                        System.out.println((i + 1) + ". " + search.get(i));
-                    }
+                    searchProduct (ss, products);
                 }
             }
         }
     }
+
+    private static void searchProduct (Scanner ss, ArrayList<String> products) {
+        String pop = ss.nextLine();
+        System.out.println("Найдено:");
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).toLowerCase().contains(pop.toLowerCase())) {
+                System.out.println((i + 1) + ". " + products.get(i));
+            }
+        }
+    }
+
+    private static void deleteProduct(Scanner ss, ArrayList<String> products) {
+        shoppingList(products);
+        System.out.println("Какую хотите удалить? Введите номер или название.");
+        String operation = ss.next();
+        try {
+            int number = Integer.parseInt(operation);
+            products.remove(number - 1);
+        } catch (Exception e) {
+            products.remove(operation);
+        }
+    }
+
+    private static void addProduct(Scanner ss, ArrayList<String> products) {
+        System.out.println("Какую покупку добавить ?");
+        products.add(ss.nextLine());
+        System.out.println("Итого в списке покупок: " + products.size());
+        System.out.println();
+    }
+
     public static void shoppingList (ArrayList < String > products) {
         System.out.println("Список покупок:");
         for (int i = 0; i < products.size(); i++) {
